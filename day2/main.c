a16189797c24bd4e0bd80ff700b2861c3f83d818
ldoney <lincoln@lincolndoney>
2020-12-09 17:18:42 -0500

    Finished day 2 puzzle 2

--- day2/main.c
+++ day2/main.c
@@ -18,17 +18,19 @@ int main() {
   char *point_buff;
   char buff[BUFF_SIZE];
   Rule rule;
-  int count, valid_count;
+  int count, puz1_count, puz2_count;
 
   fstream = fopen(INPUT, "r");
 
   point_buff = NULL;
-  valid_count = 0;
+  puz1_count = 0;
+  puz2_count = 0;
 
   while(fgets(buff, BUFF_SIZE + 1, fstream) != NULL) {
     sscanf(buff, "%d-%d %c: %[^\n]", &rule.low_bound, &rule.high_bound,
                                   &rule.character, rule.string);
     
+    /* Puzzle 1 */
     count = 0;
     point_buff = rule.string;
     while(point_buff = strchr(point_buff, rule.character)) {
@@ -36,11 +38,15 @@ int main() {
       count++;
     }
     if(count >= rule.low_bound && count <= rule.high_bound) {
-      valid_count++;
+      puz1_count++;
     }
+    
+    if(rule.string[rule.low_bound - 1] == rule.character ^
+        rule.string[rule.high_bound - 1] == rule.character)
+     puz2_count++;
   }
   fclose(fstream);
-
-  printf("%d\n", valid_count);
+  printf("%d\n", puz1_count);
+  printf("%d\n", puz2_count);
   exit(EXIT_SUCCESS);
 }
