f1d317ca714576bd7873faf5c020d1418888c4e4
Lincoln Doney <lincoln.doney@base2engineering.com>
2020-12-13 21:20:16 -0500

    Made things more complicated

--- day4/main.c
+++ day4/main.c
@@ -5,6 +5,16 @@
 
 #define INPUT "input"
 
+#define FORMAT_BYR "%d"
+#define FORMAT_IYR "%d"
+#define FORMAT_EYR "%d"
+#define FORMAT_HGT_IN "%din"
+#define FORMAT_HGT_CM "%dcm"
+#define FORMAT_HCL "#%06x"
+#define FORMAT_ECL_AMB "%s"
+#define FORMAT_PID "%09d"
+#define FORMAT_CID "%s"
+
 #define BUFF_SIZE 150
 
 typedef union {
@@ -40,15 +50,20 @@ int validate_passport(Passport p) {
 
 int main() {
   FILE *fstream;
-  char buff[BUFF_SIZE];
-  char temp_buff[BUFF_SIZE];
+  char buff[BUFF_SIZE + 1];
+  char temp_buff[BUFF_SIZE + 1];
+  char *pointer_to_loc;
   int puz1_count, puz2_count;
+  unsigned int value_from_scan;
+  char color_loop[][4] = {"amb", "blu", "brn", 
+	  					"gry", "grn", "hzl", "oth"};
   Passport p;
 
   fstream = fopen(INPUT, "r");
 
   puz1_count = 0;
   puz2_count = 0;
+  value_from_scan = 0;
 
   p.byte = 0;
 
@@ -66,22 +81,64 @@ int main() {
       /* Newline! */
       p.byte = 0;
     } else {
-      if (strstr(buff, "byr:") != NULL) 
-        p.parts.byr = 1;
-      if (strstr(buff, "iyr:") != NULL)
-        p.parts.iyr = 1;
-      if (strstr(buff, "eyr:") != NULL)
-        p.parts.eyr = 1;
-      if (strstr(buff, "hgt:") != NULL)
-        p.parts.hgt = 1;
-      if (strstr(buff, "hcl:") != NULL)
-        p.parts.hcl = 1;
-      if (strstr(buff, "ecl:") != NULL)
-        p.parts.ecl = 1;
-      if (strstr(buff, "pid:") != NULL)
-        p.parts.pid = 1;
-      if (strstr(buff, "cid:") != NULL) {
-        p.parts.cid = 1;
+		/* Oh no... */
+      if ((pointer_to_loc = strstr(buff, "byr:")) != NULL) {
+		pointer_to_loc += strlen("byr:");
+		if(sscanf(pointer_to_loc, FORMAT_BYR, &value_from_scan) == 1) {
+			if(value_from_scan >= 1920 && value_from_scan <= 2002)
+        		p.parts.byr = 1;
+		}
+	  }
+      if ((pointer_to_loc = strstr(buff, "iyr:")) != NULL) {
+		pointer_to_loc += strlen("iyr:");
+		if(sscanf(pointer_to_loc, FORMAT_IYR, &value_from_scan) == 1) {
+			if(value_from_scan >= 2010 && value_from_scan <= 2020)
+        		p.parts.iyr = 1;
+		}
+	  }
+      if ((pointer_to_loc = strstr(buff, "eyr:")) != NULL) {
+		pointer_to_loc += strlen("eyr:");
+		if(sscanf(pointer_to_loc, FORMAT_EYR, &value_from_scan) == 1) {
+			if(value_from_scan >= 2020 && value_from_scan <= 2030)
+        	  p.parts.eyr = 1;
+		}
+	  }
+      if ((pointer_to_loc = strstr(buff, "hgt:")) != NULL) {
+		pointer_to_loc += strlen("hgt:");
+		if(sscanf(pointer_to_loc, FORMAT_HGT_CM, &value_from_scan) == 1) {
+			if(value_from_scan >= 150 && value_from_scan <= 193)
+        		p.parts.hgt = 1;
+		}
+		else if(sscanf(pointer_to_loc, FORMAT_HGT_IN, &value_from_scan) == 1) {
+        	if(value_from_scan >= 59 && value_from_scan <= 76)
+				p.parts.hgt = 1;
+		}
+	  }
+      if ((pointer_to_loc = strstr(buff, "hcl:")) != NULL) {
+		pointer_to_loc += strlen("hcl:");
+		if(sscanf(pointer_to_loc, FORMAT_HCL, &value_from_scan) == 1) {
+        	p.parts.hcl = 1;
+		}
+	  }
+      if ((pointer_to_loc = strstr(buff, "ecl:")) != NULL) {
+		pointer_to_loc += strlen("ecl:");
+		for(value_from_scan = 0; value_from_scan < 7; value_from_scan++) {
+			if((strstr(pointer_to_loc, color_loop[value_from_scan])
+					   	- pointer_to_loc) <= 3)
+				p.parts.pid = 1;
+		}
+	  }
+      if ((pointer_to_loc = strstr(buff, "pid:")) != NULL) {
+		pointer_to_loc += strlen("pid:");
+		if(sscanf(pointer_to_loc, FORMAT_PID, &value_from_scan) == 1) {
+        	p.parts.ecl = 1;
+		}
+	  }
+      if ((pointer_to_loc = strstr(buff, "cid:")) != NULL) {
+		pointer_to_loc += strlen("cid:");
+		if(sscanf(pointer_to_loc, FORMAT_CID, &value_from_scan) == 1) {
+        	p.parts.cid = 1;
+		}
       }
     }
   }
@@ -91,7 +148,7 @@ int main() {
     puz1_count++;
 
   fclose(fstream);
-  printf("%d\n", puz1_count);
+  printf("\n%d\n", puz1_count);
   printf("%d\n", puz2_count);
   exit(EXIT_SUCCESS);
 }
